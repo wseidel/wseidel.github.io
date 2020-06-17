3f57573bb2a9ed94c2438a1f8b593f069a1ccac8
wseidel <wesley.seidel@gmail.com>
2020-06-17 17:39:25 -0300

    Organizando a documentação

--- publishconf.py
+++ publishconf.py
@@ -11,11 +11,12 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https//wseidel.github.io'
+# SITEURL = 'https://wseidel.github.io'
+SITEURL = 'https://wseidel.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
